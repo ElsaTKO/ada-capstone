884a8864d7e43189f8f7686ee7fa486634ef07c3
Elsa <elsa.moluf@gmail.com>
2015-10-06 15:15:45 -0700

    Added example code.

---
+++ model.js
@@ -0,0 +1,13 @@
+// var mongoose = require('mongoose'),
+//     Schema = mongoose.Schema;
+//
+// var dtSchema = new Schema({
+//     name:String,
+//     description:String,
+//     date: {type: Date, default: Date.now},
+//     longitude: Number,
+//     latitude: Number,
+//     coords: [Number, Number]
+// });
+//
+// module.exports = mongoose.model('DogTag', dtSchema);
---
+++ server.js
@@ -0,0 +1,20 @@
+// // Retrieve
+// var MongoClient = require('mongodb').MongoClient;
+//
+// // Connection
+// var url = 'mongodb://localhost:3001/cap-dev';
+//
+// // Connect
+// MongoClient.connect(url, function (err, db) {
+//   if (err) {
+//     console.log('Unable to connect to the mongoDB server. Error:', err);
+//   } else {
+//     // connected
+//     console.log('Connection established to', url);
+//
+//     // do some work here with the database.
+//
+//     // close connection
+//     db.close();
+//   }
+// });
