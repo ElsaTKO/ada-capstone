d8761573ce201344fdc3dc9a2561c20a0f045f1e
Elsa <elsa.moluf@gmail.com>
2015-10-17 11:52:06 -0700

    Done storing breweries from API in json format.

--- convert_brewery_xml_to_js.js
+++ convert_brewery_xml_to_js.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 var xml2js = require('xml2js');
-var sleep = require('sleep');
 
 var parser = new xml2js.Parser();
+
+var all_beer;
+var breweries = [];
 
 fs.readFile('./breweries_from_api_chars.xml', function(err, data) {
   if (err) {
@@ -13,7 +15,37 @@ fs.readFile('./breweries_from_api_chars.xml', function(err, data) {
         console.log(err);
       } else {
         // console.dir(result);
-        fs.writeFile('all_beer.json', JSON.stringify(result, null, 2), function(err) {
+        all_beer = (result.bmp_locations.location);
+
+        for (i = 0; i < all_beer.length; i++) {
+          // strip arrays wrapping each value
+          all_beer[i].name = all_beer[i].name[0];
+          all_beer[i].status = all_beer[i].status[0];
+          all_beer[i].street = all_beer[i].street[0];
+          all_beer[i].city = all_beer[i].city[0];
+          all_beer[i].state = all_beer[i].state[0];
+          all_beer[i].zip = all_beer[i].zip[0];
+          all_beer[i].country = all_beer[i].country[0];
+
+          // remove unwanted keys
+          delete all_beer[i].id;
+          delete all_beer[i].reviewlink;
+          delete all_beer[i].proxylink;
+          delete all_beer[i].blogmap;
+          delete all_beer[i].phone;
+          delete all_beer[i].overall;
+          delete all_beer[i].imagecount;
+
+          // remove non-breweries
+          if (all_beer[i].status === "Brewery") {
+            breweries.push(all_beer[i]);
+          }
+        }
+
+        console.log(breweries.length);
+        console.log(breweries[0]);
+
+        fs.writeFile('breweries.json', JSON.stringify(breweries, null, 2), function(err) {
           if (err) {
             console.log(err);
           } else {
