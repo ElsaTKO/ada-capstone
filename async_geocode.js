a37b7847443a2ed99c90669adcad6b437521004f
Elsa <elsa.moluf@gmail.com>
2015-10-20 16:06:36 -0700

    Index 000-050

--- async_geocode.js
+++ async_geocode.js
@@ -70,10 +70,10 @@ app.get('/google', function openConnection(req, res) {
 
   async.forEachOfSeries(old_food_trucks, function iterator(truck_json, index, iteratorCallback) {
 
-    if (index < 40) { // for truck at this index
+    if (index < 50) { // for truck at this index
       return iteratorCallback();
     }
-    if (index > 40) {
+    if (index > 50) {
       return iteratorCallback();
     }
 
