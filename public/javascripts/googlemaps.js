ad01826f1fd0eb5d29215c04f6ac709e4de60abc
Elsa <elsa.moluf@gmail.com>
2015-10-21 13:16:56 -0700

    Displaying two hard-coded markers in Seattle with map centered over Seattle. Yas.

--- public/javascripts/googlemaps.js
+++ public/javascripts/googlemaps.js
@@ -1,1 +1,19 @@
-// lol
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {lat: 47.6097, lng: -122.3331}
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 47.6220217, lng: -122.3358359},
+    map: map,
+    title: 'Hello World!'
+  });
+
+  var marker2 = new google.maps.Marker({
+    position: {lat: 47.5981458, lng: -122.3345507},
+    map: map,
+    title: 'Hello World2!'
+  });
+}
