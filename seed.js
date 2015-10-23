26a7eb00aee6eaf5f75a29c130b026cde2443ed5
Elsa <elsa.moluf@gmail.com>
2015-10-22 17:06:28 -0700

    Seeded breweries into db.

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var db = mongoose.connection;
 var assert = require('assert');
 var food_trucks = require('./FOOD_TRUCK_GEO');
-// var breweries = require('./BREWERY_DATA.JSON');
+var breweries = require('./BREWERY_GEO');
 // var distilleries = require ('./DISTILLERY_DATA.JSON');
 
 mongoose.connect('mongodb://localhost/boozeybites');
@@ -17,13 +17,13 @@ db.collection("foodtrucks").drop(function(err) {
   }
 });
 
-// db.collection("breweries").drop(function(err) {
-//   if (err) {
-//     console.log("Collection drop error: ", err);
-//   } else {
-//     console.log("breweries collection dropped.");
-//   }
-// });
+db.collection("breweries").drop(function(err) {
+  if (err) {
+    console.log("Collection drop error: ", err);
+  } else {
+    console.log("breweries collection dropped.");
+  }
+});
 
 // db.collection("distilleries").drop(function(err) {
 //   if (err) {
@@ -44,14 +44,16 @@ FoodTruck.collection.insertMany(food_trucks, function(err,res) {
        assert.equal(116, res.insertedCount);
        console.log(res.insertedCount);
 
-       db.close();
+      //  db.close();
  });
 
- // Brewery.collection.insertMany(breweries, function(err,res) {
- //        assert.equal(null, err);
- //
- //        db.close();
- //  });
+ Brewery.collection.insertMany(breweries, function(err,res) {
+        assert.equal(null, err);
+        assert.equal(34, res.insertedCount);
+        console.log(res.insertedCount);
+
+        db.close();
+  });
 
  // Distillery.collection.insertMany(distilleries, function(err,res) {
  //        assert.equal(null, err);
