87297ac1bbcebc38ca53b9a85c97d9864d9ff7e0
Elsa <elsa.moluf@gmail.com>
2015-10-22 17:15:21 -0700

    Removed old db query from index. wtf. Have been using ajax api call for a while now.

--- routes/index.js
+++ routes/index.js
@@ -5,26 +5,11 @@ dotenv.load();
 
 var GOOGLE_KEY = process.env.GOOGLE_KEY;
 
-// var MongoClient = require('mongodb').MongoClient;
-
-var json_foodtrucks;
-
-var FoodTruck = require('../models/foodtruck');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  FoodTruck.find(function(err, foodtrucks) {
-    if (err) {
-      res.send(err);
-    }
-
-    // res.json(foodtrucks);
-    json_foodtrucks = JSON.stringify(foodtrucks);
-
-    // res.render('index', { title : 'express', json: json_foodtrucks });
     res.render('index', { GOOGLE_KEY: GOOGLE_KEY });
-  });
+
 });
 
 module.exports = router;
