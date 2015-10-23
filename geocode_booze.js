50473d0aac58050dadd67266db7314ac8a322341
Elsa <elsa.moluf@gmail.com>
2015-10-22 22:56:13 -0700

    Geocoded all 12 distilleries.

--- geocode_booze.js
+++ geocode_booze.js
@@ -13,8 +13,7 @@ var new_date = new Date();
 var timestamp = new_date.toJSON();
 
 var old_breweries = require('./breweries_and_loc.json');
-var new_breweries = [];
-// var old_distilleries = require('./distilleries_and_loc.json');
+var old_distilleries = require('./distilleries_data.json');
 
 // {
 //   "name": "Georgetown Brewing Company",
@@ -29,8 +28,6 @@ var new_breweries = [];
   // (if have time) lookup google places reference?
     // (if have time) get business hours for days of week?
 
-
-
 function iterateBreweries(old_breweries) {
   for (i = 0; i < old_breweries.length; i++) {
     // DEBUGGING
@@ -41,11 +38,25 @@ function iterateBreweries(old_breweries) {
     //   return;
     // }
     new_brewery = old_breweries[i];
-    createRequestFromAddress(new_brewery, i);
+    createBreweryRequestFromAddress(new_brewery, i);
   }
 }
 
-function createRequestFromAddress(brewery, index) {
+function iterateDistilleries(old_distilleries) {
+  for (i = 0; i < old_distilleries.length; i++) {
+    // DEBUGGING
+    if (i < 10) { // FOR THIS INDEX
+      continue;
+    }
+    // if (i > 9) { // FOR THIS INDEX
+    //   return;
+    // }
+    new_distillery = old_distilleries[i];
+    createDistilleryRequestFromAddress(new_distillery, i);
+  }
+}
+
+function createBreweryRequestFromAddress(brewery, index) {
   var street = brewery.street;
   var city = brewery.city;
   var state = brewery.state;
@@ -54,20 +65,38 @@ function createRequestFromAddress(brewery, index) {
   var address_replaced = address.replace(/\s/g, "+");
   var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address_replaced + '&bounds=47.4955511,-122.4359085|47.734145,-122.2359031&key=' + GOOGLE_KEY;
 
-  hitGoogleApi(url, brewery, index);
+  hitGoogleApiForBrewery(url, brewery, index);
 }
 
-function hitGoogleApi(url, brewery, index) {
+function createDistilleryRequestFromAddress(distillery, index) {
+  var address = distillery.address;
+  var address_replaced = address.replace(/\s/g, "+");
+  var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address_replaced + '&bounds=47.4955511,-122.4359085|47.734145,-122.2359031&key=' + GOOGLE_KEY;
+
+  hitGoogleApiForDistillery(url, distillery, index);
+}
+
+function hitGoogleApiForBrewery(url, brewery, index) {
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-        addGeoToJson(body, brewery, index);
+        addBreweryGeoToJson(body, brewery, index);
     } else {
       throw error;
     }
   });
 }
 
-function addGeoToJson(body, brewery, index) {
+function hitGoogleApiForDistillery(url, distillery, index) {
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        addDistilleryGeoToJson(body, distillery, index);
+    } else {
+      throw error;
+    }
+  });
+}
+
+function addBreweryGeoToJson(body, brewery, index) {
   // console.log(data);
   var data = JSON.parse(body);
   console.log(data.results[0].formatted_address);
@@ -97,12 +126,50 @@ function addGeoToJson(body, brewery, index) {
     var lng = result.geometry.location.lng;
     var lat = result.geometry.location.lat;
     var lng_lat = [lng, lat];
-
+    // brewery.address = formatted_address;
     brewery["geometry"] = {};
     brewery.geometry["coordinates"] = lng_lat;
     brewery.geometry["type"] = "Point";
 
     writeBreweryToFile(brewery, index);
+  }
+}
+
+function addDistilleryGeoToJson(body, distillery, index) {
+  // console.log(data);
+  var data = JSON.parse(body);
+  console.log(data.results[0].formatted_address);
+  console.log(data.results[0].geometry.location.lng);
+  console.log(data.results[0].geometry.location.lat);
+  if (data.results.length > 1) {
+    distillery["geometry"] = {};
+    distillery.geometry["coordinates"] = [];
+    distillery.geometry["type"] = "Point";
+
+    var possibilities = {};
+    for (i = 0; i < data.results.length; i++) {
+      var formatted_address = data.results[i].formatted_address;
+      var lng = data.results[i].geometry.location.lng;
+      var lat = data.results[i].geometry.location.lat;
+      var lng_lat = [lng, lat];
+      possibilities[""+ data.results[i].formatted_address +""] = lng_lat;
+    }
+    console.log("*** ORIGINAL:\n", JSON.stringify(distillery, null, 2));
+    console.log("*** PLEASE CHOOSE A RESULT:\n", JSON.stringify(possibilities, null, 2));
+    writeDistilleryToFile(distillery, index);
+  } else {
+    var result = data.results[0];
+
+    var formatted_address = result.formatted_address;
+    var lng = result.geometry.location.lng;
+    var lat = result.geometry.location.lat;
+    var lng_lat = [lng, lat];
+    // distillery.address = formatted_address;
+    distillery["geometry"] = {};
+    distillery.geometry["coordinates"] = lng_lat;
+    distillery.geometry["type"] = "Point";
+
+    writeDistilleryToFile(distillery, index);
   }
 }
 
@@ -113,9 +180,17 @@ function writeBreweryToFile(brewery, index) {
   console.log("\nDONE: BREWERY " + index);
 }
 
+function writeDistilleryToFile(distillery, index) {
+  fs.writeFile('d' + index + "_" + timestamp + '.json', JSON.stringify(distillery, null, 2), function (err) {
+    if (err) throw err;
+  });
+  console.log("\nDONE: DISTILLERY " + index);
+}
+
 app.get('/google', function openConnection(req, res) {
 
-  iterateBreweries(old_breweries);
+  // iterateBreweries(old_breweries);
+  iterateDistilleries(old_distilleries);
 
 });
 
